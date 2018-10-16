47c4d537a015a58c83bbdb232b1505b70416785c
Student <student@imac-01.local>
2018-10-16 12:28:06 +0200

    Min Temp assignment to local variable works

--- PrognozaPogody/ViewController.swift
+++ PrognozaPogody/ViewController.swift
@@ -14,23 +14,33 @@ class ViewController: UIViewController {
     
     let WOEID_WARSAW: String = "523920"
     
+    var Weathers: [DayWeather]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         let dateStr = "2018/10/21"
         
         print("getting data from api on \(dateStr)")
-        getWeatherInformation("https://www.metaweather.com/api/location/\(WOEID_WARSAW)/\(dateStr)/", infoContainer)
+        getWeatherInfoByDate(date: "2018/10/21")
         
         //Gather information from rest API for hardCoded localisation
         // Display those in a view inside the app
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    func getWeatherInformation(_ url_str: String, _ label: UILabel) {
+    /*
+     * Date format is yyyy/mm/dd
+     */
+    func getWeatherInfoByDate(date: String)  -> DayWeather {
+        return getWeatherInformation("https://www.metaweather.com/api/location/\(WOEID_WARSAW)/\(date)/", infoContainer)
+    }
+    
+    func getWeatherInformation(_ url_str: String, _ label: UILabel) -> DayWeather {
         print("GET \(url_str)")
         let url: URL = URL(string: url_str)!
         let session = URLSession.shared
+        var dayWeather = DayWeather()
         var infoString: String = ""
         let task = session.dataTask(with: url, completionHandler: {
             (data, response, error)
@@ -38,10 +48,15 @@ class ViewController: UIViewController {
             if data != nil {
                 do{
                     let json = try JSONSerialization.jsonObject(with: data!, options: []) as! [[String: Any]]
-                    
+                    // Assumption that latestForecast["min_temp"] returns NSNumber (actually, NSCFNumber)
                     let latestForecast = json[0]
-                    infoString = latestForecast["min_temp"] as! String + "is the minimal temperature on \(latestForecast["applicable_date"] as! String)"
-                    
+                    if let minTemp = latestForecast["min_temp"] as? NSNumber {
+                        infoString =  minTemp.stringValue + "is the minimal temperature on \(latestForecast["applicable_date"] as! String)"
+                        dayWeather.min_temp = minTemp
+//                        DispatchQueue.main.async {
+//                            label.text = minTemp.stringValue
+//                        }
+                    }
                     print(infoString)
                 }
                 catch
@@ -53,12 +68,8 @@ class ViewController: UIViewController {
             }
         })
         task.resume()
-        DispatchQueue.main.async {
-            label.text = infoString
-        }
+        return dayWeather
+        
     }
-    
-    
-
 }
 
---
+++ PrognozaPogody/WeatherModel.swift
@@ -0,0 +1,13 @@
+//
+//  WeatherModel.swift
+//  PrognozaPogody
+//
+//  Created by Student on 16/10/2018.
+//  Copyright © 2018 IHaveAMacNow. All rights reserved.
+//
+
+import UIKit
+
+class WeatherModel: NSObject {
+
+}
